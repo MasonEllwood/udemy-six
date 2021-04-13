73d46cde9f21ea7791db090d118c648d5b965ca7
Mason Ellwood <masonellwood@gmail.com>
2021-04-13 08:57:21 -0700

    dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 const items = [
     {
@@ -20,6 +21,7 @@ const items = [
 export default () => {
     return (
         // <h1><Accordion items={items} /></h1>
-        <h1><Search /></h1>
+        // <h1><Search /></h1>
+        <h1><Dropdown /></h1>
     );
 }
