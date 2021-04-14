28e12cc925fbffebcd49df8959626eb36b01a0c1
Mason Ellwood <masonellwood@gmail.com>
2021-04-14 10:23:03 -0700

    stonks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,57 @@
-import React from 'react';
-import Accordion from './components/Accordion';
-import Search from './components/Search';
+import React, { useState } from 'react';
+// import Accordion from './components/Accordion';
+// import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 
-const items = [
+// accordian model
+// const items = [
+//     {
+//         title: 'What is react?',
+//         content: 'React is mad lame and kinda sucks'
+//     },
+//     {
+//         title: 'Why react?',
+//         content: 'because boss doesnt like Flutter'
+//     },
+//     {
+//         title: 'Some react?',
+//         content: 'React AKA lame city'
+//     }
+// ];
+
+const options = [
     {
-        title: 'What is react?',
-        content: 'React is mad lame and kinda sucks'
+        label: 'The Color Red',
+        value: 'red'
     },
     {
-        title: 'Why react?',
-        content: 'because boss doesnt like Flutter'
+        label: 'The Color Blue',
+        value: 'blue'
     },
     {
-        title: 'Some react?',
-        content: 'React AKA lame city'
-    }
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Orange',
+        value: 'orange'
+    },
+    {
+        label: 'The Color Yellow',
+        value: 'yellow'
+    },
 ];
 
 export default () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         // <h1><Accordion items={items} /></h1>
         // <h1><Search /></h1>
-        <h1><Dropdown /></h1>
+        <Dropdown 
+            selected={selected}
+            onSelectedChange={setSelected}
+            options={options}
+        />
     );
 }
