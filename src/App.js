1b8b68c5605f8d6aa753541a59900631687e4dac
Mason Ellwood <masonellwood@gmail.com>
2021-04-19 12:49:59 -0700

    sync

--- src/App.js
+++ src/App.js
@@ -1,67 +1,72 @@
 import React, { useState } from 'react';
-// import Accordion from './components/Accordion';
-// import Search from './components/Search';
-// import Dropdown from './components/Dropdown';
+import Accordion from './components/Accordion';
+import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 // accordian model
-// const items = [
-//     {
-//         title: 'What is react?',
-//         content: 'React is mad lame and kinda sucks'
-//     },
-//     {
-//         title: 'Why react?',
-//         content: 'because boss doesnt like Flutter'
-//     },
-//     {
-//         title: 'Some react?',
-//         content: 'React AKA lame city'
-//     }
-// ];
+const items = [
+    {
+        title: 'What is react?',
+        content: 'React is mad lame and kinda sucks'
+    },
+    {
+        title: 'Why react?',
+        content: 'because boss doesnt like Flutter'
+    },
+    {
+        title: 'Some react?',
+        content: 'React AKA lame city'
+    }
+];
 
-// const options = [
-//     {
-//         label: 'The Color Red',
-//         value: 'red'
-//     },
-//     {
-//         label: 'The Color Blue',
-//         value: 'blue'
-//     },
-//     {
-//         label: 'The Color Green',
-//         value: 'green'
-//     },
-//     {
-//         label: 'The Color Orange',
-//         value: 'orange'
-//     },
-//     {
-//         label: 'The Color Yellow',
-//         value: 'yellow'
-//     },
-// ];
+const options = [
+    {
+        label: 'The Color Red',
+        value: 'red'
+    },
+    {
+        label: 'The Color Blue',
+        value: 'blue'
+    },
+    {
+        label: 'The Color Green',
+        value: 'green'
+    },
+    {
+        label: 'The Color Orange',
+        value: 'orange'
+    },
+    {
+        label: 'The Color Yellow',
+        value: 'yellow'
+    },
+];
 
 export default () => {
-    // const [selected, setSelected] = useState(options[0]);
-    // const [showDropdown, setShowDropdown] = useState(true);
-
+    const [selected, setSelected] = useState(options[0])
     return (
         <div>
-            <Translate />
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}    
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
-        // <h1><Accordion items={items} /></h1>
-        // <h1><Search /></h1>
-        // <div>
-        //     <button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
-        //     {showDropdown ?
-        //         <Dropdown 
-        //             selected={selected}
-        //             onSelectedChange={setSelected}
-        //             options={options}
-        //         /> : null
-        //     }
-        // </div>
     );
 }
